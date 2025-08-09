ef1fe7872a64fc5b1668d5c8264bbf06501f712d
Vaibhav.rathi07 <vaibhavrathi140@gmail.com>
2025-08-10 01:25:23 +0530

    change

--- Server/index.js
+++ Server/index.js
@@ -76,7 +76,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
